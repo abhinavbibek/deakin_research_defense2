c7b8662c145551922cc3f991b5715092a89e43b4
Bibek Bhandari <110053125+abhinavbibek@users.noreply.github.com>
2026-02-09 17:42:32 +0545

    Update run_gtsrb.sh

--- run_gtsrb.sh
+++ run_gtsrb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ## Train ASD model under BadNets attack for GTSRB
-#python ASD.py --config config/baseline_asd_gtsrb.yaml --resume False --gpu 1
+python ASD.py --config config/baseline_asd_gtsrb.yaml --resume False --gpu 1
 
 ## Test ASD model under BadNets attack for GTSRB
-#python test.py --config config/baseline_asd_gtsrb.yaml --resume latest_model.pt --gpu 1
+python test.py --config config/baseline_asd_gtsrb.yaml --resume latest_model.pt --gpu 1
 
 
 ## Training No Defense model under BadNets attack for GTSRB
