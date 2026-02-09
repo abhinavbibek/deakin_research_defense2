b9cdad10e818fe18dad9c1ad584c3188faafbe8a
Bibek Bhandari <110053125+abhinavbibek@users.noreply.github.com>
2026-02-09 17:42:08 +0545

    Update run_cifar.sh

--- run_cifar.sh
+++ run_cifar.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 ## Train ASD model under BadNets attack for CIFAR10
-# python ASD.py \
-#   --config config/baseline_asd.yaml \
-#   --resume False \
-#   --gpu 1
+python ASD.py \
+  --config config/baseline_asd_cifar10.yaml \
+  --resume False \
+  --gpu 1
 
 
 ## Test ASD model under BadNets attack for CIFAR10
-# python test.py --config config/baseline_asd.yaml --resume latest_model.pt --gpu 1
+python test.py --config config/baseline_asd_cifar10.yaml --resume latest_model.pt --gpu 1
 
 
 ## Training No Defense model under BadNets attack for CIFAR10
-python train_baseline.py --config config/baseline_asd.yaml --gpu 1
+python train_baseline.py --config config/baseline_asd_cifar10.yaml --gpu 1
